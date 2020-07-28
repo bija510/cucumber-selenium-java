f32ad8dfea43c1fc3a490a792f723332391454b4
bija510 <error.zxz@gmail.com>
2020-07-28 10:24:08 -0400

    Working-Version -->Background & DataTable

--- src/test/java/Com/basic/backgroundSD/BackgroundStepDef.java
+++ src/test/java/Com/basic/backgroundSD/BackgroundStepDef.java
@@ -12,6 +12,7 @@ import cucumber.api.java.en.But;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BackgroundStepDef {
  
@@ -19,30 +20,30 @@ public class BackgroundStepDef {
 	
 	@Given("^User need to be on facebook login page$")     //^ and $ so that it will match exactly with feature file
 	public void user_need_to_be_on_facebook_login_page() throws InterruptedException {
-		System.setProperty("webdriver.chrome.driver","C:\\Users\\Bijaya Chhetri\\eclipse-workspace\\CucumberAuto\\driver\\chromedriver.exe");
+		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
 		driver.get("https://www.facebook.com/");
 		Thread.sleep(1000);
 	}
 	@When("^User enter user \"([^\"]*)\" first name$")
 	public void user_enter_user_first_name(String userName) throws InterruptedException {
-		driver.findElement(By.xpath("//input[@id='u_0_f']")).sendKeys(userName);
+		driver.findElement(By.xpath("//input[@id='u_0_m']")).sendKeys(userName);
         Thread.sleep(1000);
 	} 
 	@Then("^User checks user \"([^\"]*)\" first name is present$")
 	public void user_checks_user_first_name_is_present(String userName) throws InterruptedException {
-	String userNameActual=driver.findElement(By.xpath("//input[@id='u_0_f']")).getAttribute("value");
+	String userNameActual=driver.findElement(By.xpath("//input[@id='u_0_m']")).getAttribute("value");
 	Assert.assertEquals(userName,userNameActual ); //take object
 	 Thread.sleep(1000);
 	}
 	@When("^User enter user \"([^\"]*)\" lastname$")
 	 public void user_enters_user_first_name(String lastName) throws InterruptedException { //s
-		 driver.findElement(By.xpath("//input[@id='u_0_h']")).sendKeys(lastName);
+		 driver.findElement(By.xpath("//input[@id='u_0_o']")).sendKeys(lastName);
 		 Thread.sleep(1000);
 	 }
 	 @Then("User mobile fields should be blank")
 	 public void user_mobile_fields_should_be_blank() {
-		 String mobileActual =driver.findElement(By.xpath("//input[@id='u_0_k']")).getAttribute("value");
+		 String mobileActual =driver.findElement(By.xpath("//input[@id='u_0_r']")).getAttribute("value");
 			Assert.assertEquals("", mobileActual );
 	 }
 	 @Then("close browser")
