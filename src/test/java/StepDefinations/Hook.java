e4026bb02ed0f06a027e425e8c78bd55ac739db7
bija510 <error.zxz@gmail.com>
2020-09-21 10:39:26 -0400

    Added multi-browser in hook chrome,firefox,..& Config.prop, include.

--- src/test/java/StepDefinations/Hook.java
+++ src/test/java/StepDefinations/Hook.java
@@ -1,10 +1,19 @@
 package StepDefinations;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
 import Base.BaseUtil;
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
@@ -19,24 +28,45 @@ public class Hook extends BaseUtil{
 		this.base = base;
 	}
 	
+	public Properties prop ;
 	@Before
-	public void InitializeTest() {
-		WebDriverManager.chromedriver().setup();
-	    base.Driver = new ChromeDriver();
-	    base.Driver.manage().window().maximize();
+	public void InitializeTest() throws IOException{
+		/***********************************************
+		 * WebDriverManager.chromedriver().setup(); 
+		 * base.Driver = new ChromeDriver();
+		 * base.Driver.manage().window().maximize();
+		 ***********************************************/
+		//Reading data_properties file from Data Folder
+		prop =new Properties();         
+		FileInputStream fis = new FileInputStream("../CucumberAuto/Configurations/Config.properties");
+		prop.load(fis);
+		String browserName = prop.getProperty("browser"); //CHROME
+
+		if(browserName.equalsIgnoreCase("CHROME")) {
+			WebDriverManager.chromedriver().setup();
+			base.Driver = new ChromeDriver();
+
+		}else if(browserName.equalsIgnoreCase("FIREFOX")) {
+			WebDriverManager.firefoxdriver().setup();
+			base.Driver = new FirefoxDriver();
+
+		}else if(browserName.equalsIgnoreCase("IE")) {
+			WebDriverManager.iedriver().setup();
+			base.Driver = new InternetExplorerDriver();
+			
+		}else if(browserName.equalsIgnoreCase("EDGE")) {
+			WebDriverManager.edgedriver().setup();
+			base.Driver = new EdgeDriver();
+		}
+		base.Driver.manage().window().maximize();
+		base.Driver.manage().timeouts().implicitlyWait(9, TimeUnit.SECONDS);
 	}
 	
-	
 	@After
-	public void teardown(Scenario scenario) {
+	public void teardown(Scenario scenario) throws IOException {
 		if (scenario.isFailed()) {
-			try {
-				File src = ((TakesScreenshot) Driver).getScreenshotAs(OutputType.FILE);
-				FileUtils.copyFile(src, new File(
-						System.getProperty("user.dir") + "//Screenshots//" + "Failed_" + scenario.getName() + ".png"));
-			} catch (Exception e) {
-
-			}
+			File src = ((TakesScreenshot) base.Driver).getScreenshotAs(OutputType.FILE);
+			FileUtils.copyFile(src, new File("../CucumberAuto/Screenshots/" + "Failed_" + scenario.getName() + ".png"));		
 		}
 		base.Driver.quit();
 	}
